bcf07f45f675b2c363bbf1c97e57630c68f50e95
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-27 14:24:07 +0100

    add upload to toyswap button

--- app/src/main/java/com/example/cameratest/CameraActivity.java
+++ app/src/main/java/com/example/cameratest/CameraActivity.java
@@ -7,12 +7,16 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -21,14 +25,18 @@ public class CameraActivity extends AppCompatActivity {
 
     Button buttonGallery;
 
+    Button buttonUpload;
+
+    Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
         imageView = findViewById(R.id.cameraImage);
-        buttonCamera = findViewById(R.id.buttonCamera);
 
+        buttonCamera = findViewById(R.id.buttonCamera);
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,11 +47,18 @@ public class CameraActivity extends AppCompatActivity {
 
 
         buttonGallery = findViewById(R.id.buttonGallery);
-
         buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openGallery(view);
+            }
+        });
+
+        buttonUpload = findViewById(R.id.buttonSelectFromGallery);
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage(view);
             }
         });
 
@@ -63,8 +78,7 @@ public class CameraActivity extends AppCompatActivity {
                             if (result.getResultCode() == RESULT_OK) {
                                 assert result.getData() != null;
                                 Bundle bundle = result.getData().getExtras();
-                                Bitmap bitmap = (Bitmap) bundle.get("data");
-                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, null);
+                                bitmap = (Bitmap) bundle.get("data");
                                 imageView.setImageBitmap(bitmap);
                             }
                         }
@@ -82,6 +96,29 @@ public class CameraActivity extends AppCompatActivity {
                     assert result.getData() != null;
                     Uri imageUri = result.getData().getData();
                     imageView.setImageURI(imageUri);
+                }
+            });
+
+    public void uploadImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        uploadLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> uploadLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri imageUri = data.getData();
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                            imageView.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
             });
 
